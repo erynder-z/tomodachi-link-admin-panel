c1c9c824b8c4fb6e85034d362c8493c4d6e65b20
erynder-z <stfn.bgr@gmail.com>
2023-12-30 14:10:02 +0100

    Update 404-page

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function NotFoundPage() {
-    return <div className="w-full bg-black text-white">Not Found</div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+  return (
+    <div className="flex items-center justify-center h-screen bg-neutral-50">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
+        <p className="text-xl text-gray-600 mb-8">Oops! Page not found.</p>
+        <p
+          onClick={handleClick}
+          className="text-blue-500 hover:underline cursor-pointer"
+        >
+          Go back
+        </p>
+      </div>
+    </div>
+  );
 }
