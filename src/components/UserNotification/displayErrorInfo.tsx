6137251dd341c5a2ffa2025eac5e01d19429ea74
erynder-z <stfn.bgr@gmail.com>
2024-04-07 20:37:12 +0200

    Fix imports

--- src/components/UserNotification/displayErrorInfo.tsx
+++ src/components/UserNotification/displayErrorInfo.tsx
@@ -1,14 +1,14 @@
-import { InfoType } from '../../../types/infoTypes';
+import { InfoType } from '../../types/infoTypes';
 
 export const displayErrorInfo = (
-    setInfo: (info: InfoType | null) => void,
-    errorMessage: string,
-    emoji: string
+  setInfo: (info: InfoType | null) => void,
+  errorMessage: string,
+  emoji: string
 ) => {
-    const failedInfo = {
-        typeOfInfo: 'bad',
-        message: errorMessage,
-        icon: emoji,
-    };
-    setInfo(failedInfo as InfoType);
+  const failedInfo = {
+    typeOfInfo: 'bad',
+    message: errorMessage,
+    icon: emoji,
+  };
+  setInfo(failedInfo as InfoType);
 };
--- src/types/postTypes.ts
+++ src/types/postTypes.ts
@@ -1,27 +1,27 @@
 import { CommentType } from './commentTypes';
-import { ImageType } from './miscTypes';
+import { ImageType } from './ImageType';
 
 export type PostType = {
+  _id: string;
+  owner: {
     _id: string;
-    owner: {
-        _id: string;
-        firstName: string;
-        lastName: string;
-        userpic: ImageType;
-    };
-    text: string;
-    image?: any;
-    gifUrl: string;
-    embeddedVideoID?: string;
-    comments: CommentType[];
-    reactions: { positive: number; negative: number };
-    createdAt: Date;
-    updatedAt: Date;
+    firstName: string;
+    lastName: string;
+    userpic: ImageType;
+  };
+  text: string;
+  image?: any;
+  gifUrl: string;
+  embeddedVideoID?: string;
+  comments: CommentType[];
+  reactions: { positive: number; negative: number };
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export type MinimalPostType = {
-    _id: string;
-    owner: { _id: string };
-    createdAt: Date;
-    updatedAt: Date;
+  _id: string;
+  owner: { _id: string };
+  createdAt: Date;
+  updatedAt: Date;
 };
