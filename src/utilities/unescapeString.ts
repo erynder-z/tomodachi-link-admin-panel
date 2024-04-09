3112e338d8e096c35cc58b63d1cc56f1f4984843
erynder-z <stfn.bgr@gmail.com>
2024-04-09 22:05:50 +0200

    Ignore parameter
    This prevents a build failure

--- src/utilities/unescapeString.ts
+++ src/utilities/unescapeString.ts
@@ -1,5 +1,5 @@
 export function unescapeString(escapedString: string): string {
-  return escapedString.replace(/&#x([0-9A-Fa-f]+);/g, (match, hex) =>
+  return escapedString.replace(/&#x([0-9A-Fa-f]+);/g, (_match, hex) =>
     String.fromCharCode(parseInt(hex, 16))
   );
 }
